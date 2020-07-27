b3a5bdfd261187b5795ca6f9866957fea376cd5c
Bob Jansen <bobjansen@gmail.com>
2020-07-27 21:33:26 +0200

    Add README.md, created from vignette

---
+++ README.md
@@ -0,0 +1,4 @@
+Introduction
+============
+
+This document describes how to use Rcpp Modules.
---
+++ make.sh
@@ -0,0 +1,1 @@
+Rscript -e "rmarkdown::render('vignettes/Rcpp_Modules_HOWTO.Rmd', rmarkdown::md_document(), output_file = '../README.md')"
