03abf7b6533f1a8ffd37aa1da4882fbd1316077f
Bob Jansen <bobjansen@gmail.com>
2021-01-25 20:01:32 +0100

    Add Step3

--- src/RcppModulesExample_types.h
+++ src/RcppModulesExample_types.h
@@ -1,9 +1,9 @@
 #include "step1.h"
 #include "step2.h"
+#include "step3.h"
 
 RCPP_MODULE(step1_module) {
   function("echo", &echo);
-
 }
 
 RCPP_MODULE(step2_module) {
@@ -13,3 +13,20 @@ RCPP_MODULE(step2_module) {
   .method("print", &Echo::print)
   ;
 };
+
+RCPP_MODULE(step3_module) {
+  function("combineStrings", &combineStrings);
+  function("addNumbers", &addNumbers);
+
+  class_<StringMultiplier>("StringMultiplier")
+    .constructor<std::string>()
+    .method("add", &StringMultiplier::add)
+    .method("multiply", &StringMultiplier::multiply)
+  ;
+
+  class_<NumberMultiplier>("NumberMultiplier")
+    .constructor<int>()
+    .method("add", &NumberMultiplier::add)
+    .method("multiply", &NumberMultiplier::multiply)
+  ;
+}
